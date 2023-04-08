496ab7411bb5c589b4c961d3863504cfa0278d24
Rachel <gggggrea@gmail.com>
2023-04-08 23:11:00 +0100

    Display capital city image on the screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import fetchCountries from "./api/fetchCountries";
+import fetchCityImage from "./api/fetchCityImage";
 
 function App(response) {
   const [countries, setCountries] = useState();
   const [country, setCountry] = useState("United Kingdom");
   const [capital, setcapital] = useState("London");
+  const [cityImage, setCityImage] = useState("London");
 
   useEffect(() => {
     fetchCountries().then((res) => {
@@ -12,16 +14,23 @@ function App(response) {
     });
   }, []);
 
+  useEffect(() => {
+    fetchCityImage(capital).then((res) => {
+      setCityImage(res);
+    });
+  }, [capital]);
+
   const handleClick = () => {
     if (!countries) return;
 
     const i = Math.trunc(Math.random() * countries.data.length);
     setCountry(countries.data[i].name.common);
-    setcapital(countries.data[i].capital);
+    setcapital(countries.data[i].capital[0]);
   };
 
   return (
     <div>
+      <img alt={capital} src={cityImage} />
       <div>Country: {country}</div>
       <div>Capital: {capital}</div>
       <div>
