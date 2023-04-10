f04d349ebf0d6135b9799c98a16134bbda9c4397
Rachel <gggggrea@gmail.com>
2023-04-10 13:56:45 +0100

    [refactor] use React Query

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import fetchCountries from "./api/fetchCountries";
 import fetchCityImage from "./api/fetchCityImage";
 import fetchWeather from "./api/fetchWeather";
 import { GridLoader } from "react-spinners";
+import { useQuery, useIsFetching } from "@tanstack/react-query";
 
 function App(response) {
-  const [loading, setLoading] = useState(false);
-
-  const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("United Kingdom");
   const [capital, setCapital] = useState("London");
-  const [cityImage, setCityImage] = useState("London");
-  const [weather, setWeather] = useState({});
 
-  const weatherDesc = weather.current?.condition.text;
+  const { data: countries } = useQuery({
+    queryKey: ["countries"],
+    queryFn: fetchCountries,
+    onError: () => alert("Couldnt fetch a list of country."),
+  });
 
-  useEffect(() => {
-    setLoading(true);
-    fetchCountries().then((res) => {
-      setCountries(res);
-      setLoading(false);
-    });
-  }, []);
+  const { data: weather } = useQuery({
+    queryKey: ["weather", capital],
+    queryFn: () => fetchWeather(capital),
+    onError: () => alert(`Couldnt fetch the weather data for ${capital}`),
+    select: (weather) => {
+      const desc = weather?.current?.condition.text;
+      const iconSrc = `https:${weather.current?.condition.icon}`;
+      const localTime = weather.location?.localtime;
 
-  useEffect(() => {
-    setLoading(true);
-    fetchWeather(capital).then((res) => {
-      setWeather(res);
-      fetchCityImage(capital).then((res) => {
-        setCityImage(res);
-        setLoading(false);
-      });
-    });
-  }, [capital]);
+      return { desc, iconSrc, localTime };
+    },
+  });
+
+  const { data: cityImage } = useQuery({
+    queryKey: ["city image", capital],
+    queryFn: () => fetchCityImage(capital),
+    onError: () => alert(`Couldnt fetch any images for ${capital}.`),
+  });
 
   const handleClick = () => {
     if (!countries) return;
@@ -42,7 +42,9 @@ function App(response) {
     setCapital(countries.data[i].capital[0]);
   };
 
-  if (loading) return <GridLoader color="#578ce6" margin={4} />;
+  const isFetching = useIsFetching();
+  if (isFetching) return <GridLoader color="#578ce6" margin={4} />;
+  if (!weather) return null;
 
   return (
     <div>
@@ -51,10 +53,10 @@ function App(response) {
       <div>Capital: {capital}</div>
       <div>
         Weather:
-        <img alt={weatherDesc} src={`https:${weather.current?.condition.icon}`} />
-        {weatherDesc}
+        <img alt={weather.desc} src={weather.iconSrc} />
+        {weather.desc}
       </div>
-      <div>Local Time: {weather.location?.localtime} </div>
+      <div>Local Time: {weather?.localTime} </div>
       <div>
         <button onClick={handleClick}> Next </button>
       </div>
