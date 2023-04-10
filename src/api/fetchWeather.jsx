6aff10ad3691010ee55c348169c782c13cb5436b
Onur Ozcan <onur@9fin.com>
2023-04-10 16:48:57 +0100

    Use https for weather api url

--- src/api/fetchWeather.jsx
+++ src/api/fetchWeather.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 async function fetchWeather(capital) {
   const response = await axios.get(
-    `http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${capital}&aqi=no`
+    `https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_WEATHER_API_KEY}&q=${capital}&aqi=no`
   );
 
   return response.data;
